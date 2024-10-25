3831111178767b71dec6d2454bd0870123f14372
nqloi <loinq.dev@gmail.com>
2024-10-25 23:02:44 +0700

    resolve conflic

--- Client/src/App.vue
+++ Client/src/App.vue
@@ -14,5 +14,4 @@ import { RouterLink, RouterView } from 'vue-router'
   <RouterView />
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
--- Client/src/views/Login/Login.vue
+++
@@ -1,9 +0,0 @@
-<template lang="">
-    <div class="h-full text-primary">
-        hello world
-    </div>
-</template>
-<script setup>
-</script>
-<style lang="scss">
-</style>
