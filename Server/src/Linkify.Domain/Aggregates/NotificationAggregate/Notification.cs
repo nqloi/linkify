3ea1584ce0167678d9235ea0a49128efde809d19
nqloi <loinq.dev@gmail.com>
2025-03-11 23:28:33 +0700

    feat: Integrate SignalR for chat and notifications
    Added SignalR integration in the frontend for chat and notification services. Introduced `IChatService` and `INotificationService` interfaces. Created `Notification` entity and `NotificationType` enum. Configured `Notification` entity in `NotificationConfiguration.cs`. Implemented `ChatHub` and `NotificationHub` for real-time communication. Developed `SignalRChatService` and `SignalRNotificationService` classes. Added DI configuration for SignalR services.

---
+++ Server/src/Linkify.Domain/Aggregates/NotificationAggregate/Notification.cs
@@ -0,0 +1,35 @@
+﻿using Linkify.Domain.Bases;
+using Linkify.Domain.Enums.Notification;
+using Linkify.Domain.Interfaces;
+
+namespace Linkify.Domain.Aggregates.NotificationAggregate
+{
+    public class Notification : BaseEntity, IAggregateRoot
+    {
+        public Guid UserId { get; private set; }
+        public string Title { get; private set; }
+        public string Message { get; private set; }
+        public NotificationType Type { get; private set; }
+        public bool IsRead { get; private set; }
+        public DateTime CreatedAt { get; private set; }
+        public string? ActionUrl { get; private set; }
+
+        private Notification() { }
+
+        public Notification(Guid userId, string title, string message, NotificationType type, string? actionUrl = null) : base()
+        {
+            UserId = userId;
+            Title = title;
+            Message = message;
+            Type = type;
+            IsRead = false;
+            ActionUrl = actionUrl;
+            CreatedAt = DateTime.UtcNow;
+        }
+
+        public void MarkAsRead()
+        {
+            IsRead = true;
+        }
+    }
+}
